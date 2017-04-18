ad70d2a1439a2d7c88e215f95d5c9f666dc1f27f
Administrator <zellersje10@mansfield.edu>
2017-04-18 09:21:08 -0400

    Updated Delete and Find

--- src/linkedlist/LinkedList.java
+++ src/linkedlist/LinkedList.java
@@ -17,17 +17,22 @@ public class LinkedList {
         return (first == null); //looks at link named first. if it's null.. returns "true"
     }
 
-    public void insertFirst(int index, double data) {
+    public void push(int index, double data) { //pushe element onto the top of list. Insert first
         Link newLink = new Link(index, data); //creates this new link.. and then the next link becomes null
         newLink.next = first;
         first = newLink;
         //give it index (int index) and data (our object or data) we want to insert first
     }
 
-    public Link deleteFirst() {
+    public Link pop() {       //delete first element in list
         Link temp = first;
         first = first.next;
         return temp;
+    }
+
+    public Link peek() {       //shows you first element in list
+
+        return first;
     }
 
     public Link find(double key) {
@@ -81,9 +86,11 @@ public class LinkedList {
 
     public static void main(String[] args) {
         LinkedList list = new LinkedList();
-        list.insertFirst(1, 25.1);
-        list.insertFirst(2, 39.2);
-        list.insertFirst(3, 10.3);
+        list.push(1, 25.1);
+        list.push(2, 39.2);
+        list.push(3, 10.3);
+        list.push(4, 2.5);
+        list.push(5, 23.2);
         System.out.println("The list of double values is\n" + list);
 //----------------------------Find Section Main---------------------------------
         System.out.println("Math for item 39.2 is: " + list.find(39.2));
@@ -91,20 +98,21 @@ public class LinkedList {
         System.out.println("Math for item 10.3 is: " + list.find(10.3));
         System.out.println("Math for item 0 is: " + list.find(0));
         System.out.println("\n");
-        
-//---------------------------Delete Section Main-------------------------------        
 
+//----------------------------Pop Section Main---------------------------------
+        Link link = list.pop();
+        System.out.println("Pop = " + link);
+        System.out.println(list);
+
+//---------------------------Delete Section Main-------------------------------        
         if (list.delete(0) != null) {
             System.out.println("Item deleted. New list is: " + "\n" + list);
         } else {
             System.out.println("Null value on delete");
         }
 
-        if (list.delete(1) != null) {
-            System.out.println("Item deleted. New list is: " + "\n" + list);
-        } else {
-            System.out.println("Null value on delete");
-        }
+//----------------------------Peek Section Main---------------------------------     
+        System.out.print("The first element in the list is: " + list.peek() + "\n");
 
     }
 
